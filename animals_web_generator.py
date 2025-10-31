488e119f315d74b8bb9a9fe8e7496f2b3642576a
Greg Dearing <gregdearing@Gregs-MacBook-Air.local>
2025-10-31 14:11:19 +0100

    Refactor animal serialization into a separate function

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,33 +5,39 @@ def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
+def serialize_animal(animal):
+    """Converts a single animal dictionary into an HTML list item"""
+    output = '<li class="cards__item">\n'
+
+    name = animal.get("name")
+    diet = animal.get("diet")
+    locations = animal.get("locations", [])
+    animal_type = animal.get("type")
+
+    if name:
+        output += f'  <div class="card__title">{name}</div>\n'
+
+    output += '  <p class="card__text">\n'
+    if diet:
+        output += f'      <strong>Diet:</strong> {diet}<br/>\n'
+    if locations and len(locations) > 0:
+        output += f'      <strong>Location:</strong> {locations[0]}<br/>\n'
+    if animal_type:
+        output += f'      <strong>Type:</strong> {animal_type}<br/>\n'
+    output += '  </p>\n'
+
+    output += '</li>\n\n'
+    return output
+
+
 
 def generate_animals_html(animals_data):
-    """Generates HTML card items for each animal"""
-    output = ""
+    """Generates HTML for a list of animals"""
+    output = ''
     for animal in animals_data:
-        name = animal.get("name")
-        diet = animal.get("diet")
-        locations = animal.get("locations", [])
-        animal_type = animal.get("type")
-
-        output += '<li class="cards__item">\n'
-
-        if name:
-            output += f'  <div class="card__title">{name}</div>\n'
-
-        output += '  <p class="card__text">\n'
-        if diet:
-            output += f'      <strong>Diet:</strong> {diet}<br/>\n'
-        if locations and len(locations) > 0:
-            output += f'      <strong>Location:</strong> {locations[0]}<br/>\n'
-        if animal_type:
-            output += f'      <strong>Type:</strong> {animal_type}<br/>\n'
-        output += '  </p>\n'
-
-        output += '</li>\n\n'
-
+        output += serialize_animal(animal)
     return output
+
 
 
 
